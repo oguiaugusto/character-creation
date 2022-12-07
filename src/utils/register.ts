b97a6409a51ea7f301d2b6e9f7cd69ee370482e1
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-07 19:00:11 -0300

    feat: register request function

---
+++ src/utils/register.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosError } from 'axios';
+import { IUserDTO, IUserLogged } from '../interfaces/IUser';
+
+const { REACT_APP_API_URL = 'http://localhost:3001' } = process.env;
+
+const createNewUser = async (user: IUserDTO) => {
+  try {
+    const response = await axios.post(`${REACT_APP_API_URL}/users`, user);
+    const data = response.data as IUserLogged;
+
+    return data;
+  } catch (error) {
+    const err = error as AxiosError;
+    const errorData = {
+      status: err.response?.status,
+      message: err.message,
+    };
+
+    if (err.response?.data) {
+      const { message } = err.response.data as { message: string | undefined };
+
+      if (message) {
+        errorData.message = message;
+      }
+    }
+
+    return errorData;
+  }
+};
+
+export default createNewUser;
