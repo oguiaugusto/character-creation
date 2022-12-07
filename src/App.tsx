fc775935c218368537efdc26b77aa659299358bc
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-07 19:26:19 -0300

    feat: create route /register

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Routes from './routes';
 
-const App: React.FC = () => {
-  return (
-    <div>Character Creation</div>
-  );
-};
+const App: React.FC = () => (
+  <Routes />
+);
 
 export default App;
