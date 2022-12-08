e1b29d994b87ace280c327b3fda71dafa434144b
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-12-08 13:40:17 -0300

    feat: password input with visibility icon component

---
+++ src/components/InputPassword.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import {
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  InputLabel,
+} from '@mui/material';
+import { ChangeHandler } from '../interfaces/types';
+
+type Props = {
+  handleChange: ChangeHandler;
+  handleShow: () => void;
+  showPassword: boolean;
+  id: string;
+  name: string;
+  placeholder: string;
+  label: string;
+};
+
+const InputPassword: React.FC<Props> = (props) => {
+  const {
+    handleChange,
+    handleShow,
+    showPassword,
+    id,
+    name,
+    placeholder,
+    label,
+  } = props;
+
+  return (
+    <FormControl variant="outlined" size="small">
+      <InputLabel htmlFor={ id }>{ label }</InputLabel>
+      <OutlinedInput
+        id={ id }
+        name={ name }
+        placeholder={ placeholder }
+        label={ label }
+        type={ showPassword ? 'text' : 'password' }
+        onChange={ handleChange }
+        endAdornment={ (
+          <InputAdornment position="start">
+            <IconButton onClick={ handleShow }>
+              {
+                showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />
+              }
+            </IconButton>
+          </InputAdornment>
+        ) }
+      />
+    </FormControl>
+  );
+};
+
+export default InputPassword;
